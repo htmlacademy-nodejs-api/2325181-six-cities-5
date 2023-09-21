b4f469e00c7864b279a076a9ae7578b6708fa49b
Rinat <r.z.zaripov@gmail.com>
2023-09-21 12:37:24 +0300

    добавил интерфейс команды и команду version

--- src/shared/types/user.type.ts
+++ src/shared/types/user.type.ts
@@ -2,8 +2,8 @@ import { UserLevel } from './user-level.type.js';
 
 export type UserType = {
   name: string;
-  email:  string;
-  avatarURL:  string;
+  email: string;
+  avatarURL: string;
   password: string;
   userType: UserLevel[keyof UserLevel];
 }
