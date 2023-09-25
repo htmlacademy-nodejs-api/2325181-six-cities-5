b3ff2122f779e23cf76b29eb007df8cd641cc99b
Rinat <r.z.zaripov@gmail.com>
2023-09-25 13:45:21 +0300

    реализовал функцию генерации

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,15 +1,51 @@
+import got from 'got';
+import { MockServerData } from '../../shared/types/mock-server-data.type.js';
 import { Command } from './command.interface.js';
+import { TSVOfferGenerator } from '../../shared/types/libs/file-reader/offer-generator/tsv-offer-generator.js';
+import { appendFile } from 'node:fs/promises';
 
 export class GenerateCommand implements Command {
+
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Cannot import data from ${url}`);
+    }
+  }
+
+  private async write(filePath: string, count: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    for(let i = 0; i < count; i++) {
+      await appendFile(
+        filePath,
+        `${tsvOfferGenerator.generate()}\n`,
+        {encoding: 'utf-8'}
+      );
+    }
+  }
 
   getName(): string {
     return '--generate';
   }
 
-  execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filePath, URL] = parameters;
     const offersCount = Number.parseInt(count, 10);
 
-    
+    try {
+      await this.load(URL);
+      await this.write(filePath, offersCount);
+      console.info(`File ${filePath} has been successfully created`);
+    } catch (err: unknown){
+      console.error('Cannot load data');
+
+      if (err instanceof Error) {
+        console.error(err.message);
+      }
+    }
+
   }
 }
