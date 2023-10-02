9ebbf5f3c856142fc0965e49c11845549e06916f
Rinat <r.z.zaripov@gmail.com>
2023-10-02 17:50:11 +0300

    добавил dotenv в RestConfig, config в RestApplicat

--- src/rest/main.rest.ts
+++ src/rest/main.rest.ts
@@ -1,9 +1,11 @@
+import { RestConfig } from '../shared/libs/config/rest.config.js';
 import { PinoLogger } from '../shared/libs/logger/pino.logger.js';
 import { RestApplication } from './rest.application.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
-  const application = new RestApplication(logger);
+  const config = new RestConfig(logger);
+  const application = new RestApplication(logger, config);
   await application.init();
 }
 
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,12 +1,15 @@
 import { Logger } from '../shared/libs/logger/logger.interface.js';
+import { Config } from '../shared/libs/config/config.interface.js';
 import { ApplicationMessages } from '../const.js';
 
 export class RestApplication {
   constructor(
-    private readonly logger: Logger
+    private readonly logger: Logger,
+    private readonly config: Config
   ) {}
 
   public async init() {
     this.logger.info(ApplicationMessages.Info);
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
