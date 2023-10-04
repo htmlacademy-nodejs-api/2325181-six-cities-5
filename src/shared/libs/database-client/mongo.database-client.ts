425f1786d6eab9e0a82eb375fab802d2315e954f
Rinat <r.z.zaripov@gmail.com>
2023-10-04 16:37:33 +0300

    реализовал повторное подключение

--- src/shared/libs/database-client/mongo.database-client.ts
+++ src/shared/libs/database-client/mongo.database-client.ts
@@ -1,8 +1,10 @@
 import * as Mongoose from 'mongoose';
 import { injectable, inject } from 'inversify';
+import { setTimeout } from 'node:timers/promises';
 import { DatabaseClient } from './database-client.interface.js';
 import { Logger } from '../logger/logger.interface.js';
 import { Component } from '../../types/component.enum.js';
+import { RETRY_COUNT, RETRY_TIMEOUT } from '../../../const.js';
 
 @injectable()
 export class MongoDatabaseClient implements DatabaseClient {
@@ -36,9 +38,20 @@ export class MongoDatabaseClient implements DatabaseClient {
   public async connect(uri: string): Promise<void> {
     this.checkConnectionStatus(false);
     this.logger.info('Trying to connect to MongoDB...');
-    this.mongoose = await Mongoose.connect(uri);
-    this.changeConnectionStatus();
-    this.logger.info('Database connection established');
+    let attempt = 0;
+    while (attempt < RETRY_COUNT) {
+      try {
+        this.mongoose = await Mongoose.connect(uri);
+        this.changeConnectionStatus();
+        this.logger.info('Database connection established');
+        return;
+      } catch (err) {
+        attempt++;
+        this.logger.warn(`Failed to connect MongoDB. Reconnecting (attempt ${attempt} of ${RETRY_COUNT}) ...`);
+        await setTimeout(RETRY_TIMEOUT);
+      }
+    }
+    throw new Error(`Unable to establish database connection after ${RETRY_COUNT} attempts. Please try again later`);
   }
 
   public async disconnect(): Promise<void> {
