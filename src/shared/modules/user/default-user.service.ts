f1d41c96922005b45950fd8f8299b2de4cbaf9d6
Rinat <r.z.zaripov@gmail.com>
2023-10-06 10:37:39 +0300

    добав findByEmail, findOrCreate, зваисим UserModel

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'inversify';
 import { UserService } from './user-service.interface.js';
-import { DocumentType } from '@typegoose/typegoose';
-import { UserEntity, UserModel } from './user.entity.js';
+import { DocumentType, types } from '@typegoose/typegoose';
+import { UserEntity } from './user.entity.js';
 import { CreateUserDTO } from './index.js';
 import { Logger } from '../../libs/logger/logger.interface.js';
 import { Component } from '../../types/component.enum.js';
@@ -9,23 +9,30 @@ import { Component } from '../../types/component.enum.js';
 @injectable()
 export class DefaultUserService implements UserService {
 
-  constructor (@inject(Component.Logger) private readonly logger: Logger) {}
+  constructor (
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.UserModel) private readonly userModel: types.ModelType<UserEntity>
+  ) {}
 
   public async create (dto: CreateUserDTO, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
-    const result = await UserModel.create(user);
+    const result = await this.userModel.create(user);
     this.logger.info(`New user created: ${user.email}`);
     return result;
   }
 
-  // public findByEmail(email: string): Promise<DocumentType<UserEntity>> | null {
+  public async findByEmail(email: string): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findOne({email});
+  }s
 
-  // }
-
-  // public findOrCreate(email: string, dto: createUserDTO): Promise<DocumentType<UserEntity>> {
-
-  // }
+  public async findOrCreate(dto: CreateUserDTO, salt: string): Promise<DocumentType<UserEntity>> {
+    const resultUser = await this.findByEmail(dto.email);
+    if (resultUser) {
+      return resultUser;
+    }
+    return this.create(dto, salt);
+  }
 
 
 }
--- src/shared/modules/user/user-service.interface.ts
+++ src/shared/modules/user/user-service.interface.ts
@@ -3,6 +3,6 @@ import { UserEntity, CreateUserDTO } from './index.js';
 
 export interface UserService {
   create (dto: CreateUserDTO, salt: string):Promise<DocumentType<UserEntity>>;
-  findByEmail (email: string): Promise<DocumentType<UserEntity>> | null;
-  findOrCreate (email: string, dto: CreateUserDTO): Promise<DocumentType<UserEntity>>;
+  findByEmail (email: string): Promise<DocumentType<UserEntity> | null>;
+  findOrCreate (dto: CreateUserDTO, salt: string): Promise<DocumentType<UserEntity>>;
 }
