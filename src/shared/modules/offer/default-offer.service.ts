03b6791a3117983fdea059109120da7c92fb9c94
Rinat <r.z.zaripov@gmail.com>
2023-10-09 16:06:34 +0300

    добавил интерфейс, реализацию дополн методов offer

--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -1,8 +1,9 @@
 import { types } from '@typegoose/typegoose';
 import { inject } from 'inversify';
-import { CreateOfferDTO, OfferEntity, OfferService } from './index.js';
+import { CreateOfferDTO, FavoritesOfferDTO, OfferEntity, OfferService } from './index.js';
 import { Component } from '../../types/component.enum.js';
 import { Logger } from '../../libs/logger/logger.interface.js';
+import { UpdateOfferDTO } from './update-offer.dto.js';
 
 export class DefaultOfferService implements OfferService {
 
@@ -18,6 +19,34 @@ export class DefaultOfferService implements OfferService {
   }
 
   public async findById(offerId: string): Promise<types.DocumentType<OfferEntity> | null> {
-    return this.offerModel.findById(offerId).exec();
+    return this.offerModel.findById(offerId).populate(['hostId']).exec();
+  }
+
+  public async find():Promise<types.DocumentType<OfferEntity>[]> {
+    return this.offerModel.find().populate('hostId').exec();
+  }
+
+  public async deleteById (offerId: string): Promise<types.DocumentType<OfferEntity> | null> {
+    return this.offerModel.findByIdAndDelete(offerId).exec();
+  }
+
+  public async updateById (offerId: string, dto: UpdateOfferDTO): Promise<types.DocumentType<OfferEntity> | null> {
+    return this.offerModel.findByIdAndUpdate(offerId, dto, {new: true})
+      .populate('hostId')
+      .exec();
+  }
+
+  public async findFavorites ():Promise<types.DocumentType<OfferEntity>[]> {
+    return this.offerModel.find({isFavorite: true}).populate('hostId').exec();
+  }
+
+  public async findPremium ():Promise<types.DocumentType<OfferEntity>[]> {
+    return this.offerModel.find({isPremium: true}).populate('hostId').exec();
+  }
+
+  public async addRemoveFavorites (offerId: string, dto: FavoritesOfferDTO):Promise<types.DocumentType<OfferEntity> | null> {
+    return this.offerModel.findByIdAndUpdate(offerId, dto, {new: true})
+      .populate('hostId')
+      .exec();
   }
 }
--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -1,8 +1,13 @@
 import { DocumentType } from '@typegoose/typegoose';
-import { CreateOfferDTO, OfferEntity } from './index.js';
-
+import { CreateOfferDTO, OfferEntity, FavoritesOfferDTO, UpdateOfferDTO } from './index.js';
 
 export interface OfferService {
   create(dto: CreateOfferDTO):Promise<DocumentType<OfferEntity>>;
   findById(offerId:string): Promise<DocumentType<OfferEntity> | null>;
+  find():Promise<DocumentType<OfferEntity>[]>;
+  deleteById(offerId:string):Promise<DocumentType<OfferEntity> | null>;
+  findFavorites():Promise<DocumentType<OfferEntity>[]>;
+  findPremium():Promise<DocumentType<OfferEntity>[]>;
+  updateById(offerId:string, dto: UpdateOfferDTO):Promise<DocumentType<OfferEntity> | null>;
+  addRemoveFavorites(offerId: string, dto: FavoritesOfferDTO):Promise<DocumentType<OfferEntity> | null>
 }
