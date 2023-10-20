c0a4a91ad4a4fa0604edd269e101723dc1386b62
Rinat <r.z.zaripov@gmail.com>
2023-10-20 16:14:33 +0300

    добав middleware в интерф route, base controller

--- src/shared/libs/rest/route/route.interface.ts
+++ src/shared/libs/rest/route/route.interface.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpMethod } from '../../../../const.js';
 import { ValuesType } from '../../../types/values.type.js';
+import { Middleware } from '../middleware/middleware.interface.js';
 
 export interface Route {
   path: string;
   method: ValuesType<typeof HttpMethod>;
   handler: (req: Request, res: Response, next: NextFunction) => void;
+  middlewares?: Middleware[];
 }
