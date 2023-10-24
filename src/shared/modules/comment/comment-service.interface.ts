69099c5360431c8a20a4bbdd8b3093f91cfffa39
Rinat <r.z.zaripov@gmail.com>
2023-10-24 13:22:42 +0300

    устан mime-types, multer, добав. UploadFileMW

--- src/shared/modules/comment/comment-service.interface.ts
+++ src/shared/modules/comment/comment-service.interface.ts
@@ -2,12 +2,10 @@ import { DocumentType } from '@typegoose/typegoose';
 import { CreateCommentDTO } from './create-comment.dto.js';
 import { CommentEntity } from './comment.entity.js';
 import { ReviewStatisticsType } from '../../types/review-statistics.type.js';
-import { DocumentExists } from '../../libs/rest/index.js';
 
-export interface CommentService extends DocumentExists {
+export interface CommentService {
   create(dto: CreateCommentDTO): Promise<DocumentType<CommentEntity>>;
   findByOfferId(offerId: string): Promise<DocumentType<CommentEntity>[]>;
   deleteByOfferId(offerId: string):Promise<number | null>;
   calculateAverageRatingByOfferId(offerId: string): Promise<ReviewStatisticsType>
-
 }
