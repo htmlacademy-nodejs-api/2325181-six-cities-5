7e8202e68afd647141557c3e5ad85bdfcad848b4
Rinat <r.z.zaripov@gmail.com>
2023-11-02 17:17:41 +0300

    выделил Validation и Http Exception filter

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -23,6 +23,8 @@ export class RestApplication {
     @inject(Component.OfferController) private readonly offerController: Controller,
     @inject(Component.CommentController) private readonly commentController: Controller,
     @inject(Component.AuthExceptionFilter) private readonly authExceptionFilter: ExceptionFilter,
+    @inject(Component.HttpExceptionFilter) private readonly httpExceptionFilter: ExceptionFilter,
+    @inject(Component.ValidationExceptionFilter) private readonly validationExceptionFilter: ExceptionFilter,
   ) {
     this.server = express();
   }
@@ -84,5 +86,7 @@ export class RestApplication {
   private async _initExceptionFilters() {
     this.server.use(this.appExceptionFilter.catch.bind(this.appExceptionFilter));
     this.server.use(this.authExceptionFilter.catch.bind(this.authExceptionFilter));
+    this.server.use(this.httpExceptionFilter.catch.bind(this.httpExceptionFilter));
+    this.server.use(this.validationExceptionFilter.catch.bind(this.validationExceptionFilter));
   }
 }
--- src/rest/rest.container.ts
+++ src/rest/rest.container.ts
@@ -5,7 +5,7 @@ import { PinoLogger, Logger } from '../shared/libs/logger/index.js';
 import { DatabaseClient, MongoDatabaseClient } from '../shared/libs/database-client/index.js';
 import { Component } from '../shared/types/component.enum.js';
 import { RestApplication } from './rest.application.js';
-import { AppExceptionFilter, ExceptionFilter } from '../shared/libs/rest/index.js';
+import { AppExceptionFilter, ExceptionFilter, HttpErrorExceptionFilter, ValidationExceptionFilter } from '../shared/libs/rest/index.js';
 
 export function createRestApplicationContainer() {
   const container = new Container();
@@ -14,5 +14,7 @@ export function createRestApplicationContainer() {
   container.bind<Config<RestSchemaType>>(Component.Config).to(RestConfig).inSingletonScope();
   container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
   container.bind<ExceptionFilter>(Component.ExceptionFilter).to(AppExceptionFilter).inSingletonScope();
+  container.bind<ExceptionFilter>(Component.HttpExceptionFilter).to(HttpErrorExceptionFilter).inSingletonScope();
+  container.bind<ExceptionFilter>(Component.ValidationExceptionFilter).to(ValidationExceptionFilter).inSingletonScope();
   return container;
 }
